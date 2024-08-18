c3f028f0d4d6ed17456cdcec3096a4366b51509c
svivek19 <svivek030503@gmail.com>
2024-08-18 21:14:30 +0530

    mongodb connection setup

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("log");
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+
+const db = process.env.MONGODB_URI;
+
+const port = 5000;
+
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(port, () => {
+      console.log(`Server is running on port: ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
